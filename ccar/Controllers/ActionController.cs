4314be3adbd685da4e2546a3bdd859c8437e5596
Jakub Skorupski <skorupski.jakub@gmail.com>
2019-06-16 22:53:49 +0200

    16062019 #1

---
+++ ccar/Controllers/ActionController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ccar.Models;
+
+namespace ccar.Controllers
+{
+    public class ActionController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+      public ActionResult GetData()
+        {
+            using (ccarEntities ent = new ccarEntities())
+            {
+                List<actions> actList = ent.actions.ToList<actions>();
+                return Json(new { data = actList }, JsonRequestBehavior.AllowGet); 
+            }
+        }
+    }
+}
---
+++ ccar/Models/Action.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ccar.Models;
+
+namespace ccar.Models
+{
+    public class Action
+    {
+
+        // 1:1 from DB
+        public int id { get; set; }
+        public int idReason{ get; set; }
+        public int idInitiator { get; set; }
+        public string originationDate { get; set; }
+        public int idTypeOfAction { get; set; }
+        public string problem { get; set; }
+        public string rootCause { get; set; }
+        public string correctiveAction { get; set; }
+        public int idResponsible { get; set; }
+        public string targetDate { get; set; }
+        public int idProgress { get; set; }
+        public string completionDate { get; set; }
+        public string measureEffic { get; set; }
+        public string dateOfEffic { get; set; }
+
+   
+
+        // saveToDatabase
+        public void saveToDatabase()
+        {
+            ccarEntities ent = new ccarEntities();
+            Action newAction = new Action();
+            newAction.id = id;
+            newAction.idReason = idReason;
+            //newAction.idInitiator
+        }
+
+      
+    }
+}
