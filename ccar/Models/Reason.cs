d1f4b182ef29485093449ed170131c55c344afb8
Jakub Skorupski <skorupski.jakub@gmail.com>
2019-06-17 11:01:27 +0200

    17062019 #1

---
+++ ccar/Models/Reason.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ccar.Models
+{
+    public class Reason
+    {
+        // 1:1 from DB
+        public int id { get; set; }
+        public string reason { get; set; }
+
+     
+        // List of reasons
+        public static List<Reason> fromReason (List<reasons> rList)
+        {
+            List<Reason> listReason = rList.Select(x => new Reason() { id = x.id, reason = x.reason }).ToList();
+            return listReason;
+        }
+        public static List<Reason> GetReasonList()
+        {
+            ccarEntities ent = new ccarEntities();
+            return fromReason(ent.reasons.ToList());
+        }
+
+    }
+}
