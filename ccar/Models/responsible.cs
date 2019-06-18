77dcce4fb0c62cf9ad39621678231b4b3654855c
Jakub Skorupski <skorupski.jakub@gmail.com>
2019-06-18 16:59:22 +0200

    18062019 #2

---
+++ ccar/Models/responsible.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ccar.Models
+{
+    public class initiator
+    {
+        public int id { get; set; }
+        public string firstname { get; set; }
+        public string surname { get; set; }
+        public string email { get; set; }
+
+             //      1.pobrac adres mailowy na podtsawie ID act(w modelu initaiotor), get emailadress, zwraca maila
+             //2. sprawdzanie czy nie jest null i czy jest poprawny - w metodzie
+
+
+        public static string  getEmailAdress (action act)
+        {
+            ccarEntities ent = new ccarEntities();
+            var xo = ent.users.Where(x => x.id == act.id).FirstOrDefault();
+            return xo.email;
+        }
+    }
+}
