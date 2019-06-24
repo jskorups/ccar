7ee36798ad4399960f63d4491fe1b4fcef918a25
Jakub Skorupski <skorupski.jakub@gmail.com>
2019-06-24 07:58:30 +0200

    24062019 #1

---
+++ ccar/Models/typeOfAction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ccar.Models
+{
+    public class typeAction
+    {
+        public int id { get; set; }
+        public string typeOfAction1 { get; set; }
+
+        public static List<typeOfaction> fromTypeOfAction(List<typeOfaction> toaList)
+        {
+            List<typeOfaction> listTOA = toaList.Select(x => new typeOfaction() { id = x.id, typeOfaction1 = x.typeOfaction1}).ToList();
+            return listTOA;
+        }
+        public static List<typeOfaction> GetResponsibleList()
+        {
+            ccarEntities ent = new ccarEntities();
+            return fromTypeOfAction(ent.typeOfaction.ToList());
+        }
+
+
+    }
+}
