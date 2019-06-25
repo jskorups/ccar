d6a586f74e572ec50c9aae3e22382a29c775a198
Jakub Skorupski <skorupski.jakub@gmail.com>
2019-06-25 16:09:15 +0200

    25062019  #1

---
+++ ccar/Controllers/typeOfActionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ccar.Controllers
+{
+    public class typeOfActionController : Controller
+    {
+        // GET: typeOfAction
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ccar/Models/Progress.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ccar.Models
+{
+    public class Progress
+    {
+        public int id { get; set; }
+        public string progressValue { get; set; }
+
+
+        public static List<Progress> fromProgress (List<progress> progList)
+        {
+            List<Progress> ProgList = progList.Select(x => new Progress() { id = x.id, progressValue = x.progressValue }).ToList();
+            return ProgList;
+        }
+
+        public static List<Progress> GetProgressList()
+        {
+            ccarEntities ent = new ccarEntities();
+            return fromProgress(ent.progress.ToList());
+        }
+
+
+    }
+}
