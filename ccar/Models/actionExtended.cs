b5899d308b8c71f188643073231ee3dcbd87a010
Jakub Skorupski <skorupski.jakub@gmail.com>
2019-06-28 08:03:49 +0200

    28062019 #1

---
+++ ccar/Models/actionExtended.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ccar.Models
+{
+    public class actionExtended:action
+    {
+        public override void Save()
+        {
+           
+        }
+    }
+}
