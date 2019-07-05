2b396234c5eae3becf5c3430a47d8e811f443cda
Jakub Skorupski <skorupski.jakub@gmail.com>
2019-07-05 16:15:34 +0200

    05072019 #1

---
+++ ccar/Models/ReasonModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ccar.Models
+{
+    public class ReasonModel
+    {
+        // 1:1 from DB
+        public int id { get; set; }
+        public string reason { get; set; }
+
+     
+        // List of reasons
+        public static List<ReasonModel> fromReason (List<reasons> rList)
+        {
+            List<ReasonModel> listReason = rList.Select(x => new ReasonModel() { id = x.id, reason = x.reason }).ToList();
+            return listReason;
+        }
+        public static List<ReasonModel> GetReasonList()
+        {
+            ccarEntities ent = new ccarEntities();
+            return fromReason(ent.reasons.ToList());
+        }
+
+        public ReasonModel()
+        {
+       
+        }
+
+        public ReasonModel(string Reason)
+        {
+            reason = Reason;
+        }
+
+        public static reasons ConvertFromModelToDB(ReasonModel r)
+        {
+            reasons rea = new reasons();
+            rea.id = r.id;
+            rea.reason = r.reason;
+
+            return rea;
+        }
+
+        public static ReasonModel ConvertFromDbToModel (reasons r)
+        {
+          
+            ReasonModel rea = new ReasonModel();
+            rea.id = r.id;
+            rea.reason = r.reason;
+            return rea;
+        }
+
+        public void Save()
+        {
+            if (this.id == 0)
+            {
+                ccarEntities ent = new ccarEntities();
+                ent.reasons.Add(ReasonModel.ConvertFromModelToDB(this));
+            }
+        }
+
+    }
+
+}
---
+++ ccar/Models/typeOfActionModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ccar.Models
+{
+    public class typeOfActionModel
+    {
+        public int id { get; set; }
+        public string typeOfAction1 { get; set; }
+
+        public static List<typeOfaction> fromTypeOfAction(List<typeOfaction> toaList)
+        {
+            List<typeOfaction> listTOA = toaList.Select(x => new typeOfaction() { id = x.id, typeOfaction1 = x.typeOfaction1}).ToList();
+            return listTOA;
+        }
+        public static List<typeOfaction> GetTypeOfActionList()
+        {
+            ccarEntities ent = new ccarEntities();
+            return fromTypeOfAction(ent.typeOfaction.ToList());
+        }
+
+
+    }
+}
