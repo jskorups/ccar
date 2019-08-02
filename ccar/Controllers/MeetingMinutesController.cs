eed27c1c782ec0b5b15b9b3662504084bf8f62d2
Jakub Skorupski <skorupski.jakub@gmail.com>
2019-08-02 16:01:18 +0200

    02082019 #4

---
+++ ccar/Controllers/MeetingMinutesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ccar.Models;
+
+namespace ccar.Controllers
+{
+    public class MeetingMinutesController : Controller
+    {
+        // GET: MeetingMinutes
+        public ActionResult MeetingMinutesList()
+        {
+           List<MeetingMinutesModel> meMin = new List<MeetingMinutesModel>();
+            ccarEntities ent = new ccarEntities();
+            meMin = MeetingMinutesModel.GetListOfFromDB(ent.meetingMinutes.ToList());
+            return View(meMin);
+        }
+    }
+}
