f917156c2666c90beccecc042ae1cfb65f22da14
Jakub Skorupski <skorupski.jakub@gmail.com>
2019-08-05 12:23:24 +0200

    05082019 #1

--- ccar/Scripts/MyScripts/IndexDataTable.js
+++ ccar/Scripts/MyScripts/IndexDataTable.js
@@ -38,7 +38,6 @@ $(document).ready(function () {
             "datatype": "json"
         },
         "columns": [
-
             {
                 "className": 'details-control',
                 "data": null,
@@ -108,72 +107,71 @@ $(document).ready(function () {
     });
 
 
-    // Add event listener for opening and closing details
-//    $('#actionTable tbody').on('click', 'td.details-control', function () {
-//        var tr = $(this).closest('tr');
-//        var tdi = tr.find("i.fa");
-//        var row = dataTable.row(tr);
 
-//        if (row.child.isShown()) {
-//            // This row is already open - close it
-//            row.child.hide();
-//            tr.removeClass('shown');
-//            tdi.first().removeClass('fa-minus-square');
-//            tdi.first().addClass('fa-plus-square');
-//        }
-//        else {
-//            // Open this row
-//            row.child(format(row.data())).show();
-//            tr.addClass('shown');
-//            tdi.first().removeClass('fa-plus-square');
-//            tdi.first().addClass('fa-minus-square');
-//        }
-//    });
+    $('#actionTable tbody').on('click', 'td.details-control', function () {
+    
+        var tr = $(this).closest('tr');
+        var tdi = tr.find("i.fa");
+        var row = dataTable.row(tr);
 
-//    dataTable.on("user-select", function (e, dt, type, cell, originalEvent) {
-//        if ($(cell.node()).hasClass("details-control")) {
-//            e.preventDefault();
-//        }
-//    });
-//});
+        if (row.child.isShown()) {
+            // This row is already open - close it
+            row.child.hide();
+            tr.removeClass('shown');
+     
+        }
+        else {
+            // Open this row
+            row.child(format(row.data())).show();
+            tr.addClass('shown');
+        
+        }
+    });
+
+    dataTable.on("user-select", function (e, dt, type, cell, originalEvent) {
+        if ($(cell.node()).hasClass("details-control")) {
+            e.preventDefault();
+        }
+    });
+});
 
 
 //////////////////////////////////////////////////////
 
 
 // Array to track the ids of the details displayed rows
-var detailRows = [];
+//var detailRows = [];
 
-$('#example tbody').on('click', 'tr td.details-control', function () {
-    var tr = $(this).closest('tr');
-    var row = dt.row(tr);
-    var idx = $.inArray(tr.attr('id'), detailRows);
+//$('#actionTable tbody').on('click', 'tr td.details-control', function () {
+//    var tr = $(this).closest('tr');
+//    var row = dt.row(tr);
+//    var idx = $.inArray(tr.attr('id'), detailRows);
 
-    if (row.child.isShown()) {
-        tr.removeClass('details');
-        row.child.hide();
+//    if (row.child.isShown()) {
+//        tr.removeClass('details');
+//        row.child.hide();
 
-        // Remove from the 'open' array
-        detailRows.splice(idx, 1);
-    }
-    else {
-        tr.addClass('details');
-        row.child(format(row.data())).show();
+//        // Remove from the 'open' array
+//        detailRows.splice(idx, 1);
+//    }
+//    else {
+//        tr.addClass('details');
+//        row.child(format(row.data())).show();
 
-        // Add to the 'open' array
-        if (idx === -1) {
-            detailRows.push(tr.attr('id'));
-        }
-    }
-});
+//        // Add to the 'open' array
+//        if (idx === -1) {
+//            detailRows.push(tr.attr('id'));
+//        }
+//    }
+//});
 
-// On each draw, loop over the `detailRows` array and show any child rows
-dt.on('draw', function () {
-    $.each(detailRows, function (i, id) {
-        $('#' + id + ' td.details-control').trigger('click');
-    });
-});
-} );
+//// On each draw, loop over the `detailRows` array and show any child rows
+//dt.on('draw', function () {
+//    $.each(detailRows, function (i, id) {
+//        $('#' + id + ' td.details-control').trigger('click');
+//    });
+//});
+//} );
 
 
 
