79fdb8018e820a1428cf89ddb458cd30c9711f0b
Jakub Skorupski <skorupski.jakub@gmail.com>
2019-08-08 09:48:36 +0200

    08082019 #1

---
+++ ccar/Controllers/MeetingMinutesUsersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ccar.Models;
+
+namespace ccar.Controllers
+{
+    public class MeetingMinutesUsersController : Controller
+    {
+        // GET: Project
+        public ActionResult UserstList()
+        {
+            List<MeetingMinutesUsersModel> proList = new List<MeetingMinutesUsersModel>();
+            ccarMeetingMinutesEntities ent = new ccarMeetingMinutesEntities();
+            proList = MeetingMinutesUsersModel.fromMMUsersDB(ent.mMusers.ToList());
+            return View(proList);
+        }
+
+
+
+
+    }
+}
---
+++ ccar/Models/MeetingMinutesUsersModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ccar.Models
+{
+    public class MeetingMinutesUsersModel
+    {
+        public int id { get; set; }
+        public string firstname { get; set; }
+        public string surname { get; set; }
+        public string email { get; set; }
+
+
+
+
+        // List of reasons
+        public static List<MeetingMinutesUsersModel> fromMMUsersDB(List<mMusers> uList)
+        {
+            List<MeetingMinutesUsersModel> usersList = uList.Select(x => new MeetingMinutesUsersModel() { id = x.id, firstname = x.firstname, surname = x.surname, email = x.email }).ToList();
+            return usersList;
+        }
+        public static List<MeetingMinutesUsersModel> GetReasonList()
+        {
+            ccarMeetingMinutesEntities ent = new ccarMeetingMinutesEntities();
+            return fromMMUsersDB(ent.mMusers.ToList());
+        }
+
+    }
+}
