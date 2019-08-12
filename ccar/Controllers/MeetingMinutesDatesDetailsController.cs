568cc55abbb8947a024b88dc40d22a05154c9c1d
Jakub Skorupski <skorupski.jakub@gmail.com>
2019-08-12 22:43:49 +0200

    12082019 #1

---
+++ ccar/Controllers/MeetingMinutesDatesDetailsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ccar.Models;
+
+namespace ccar.Controllers
+{
+    public class MeetingMinutesDatesDetailsController : Controller
+    {
+        // GET: Project
+        public ActionResult MeetingMinutesDetails()
+        {
+            List<MeetingMinutesDatesDetails> proList = new List<MeetingMinutesDatesDetails>();
+            ccarMeetingMinutesEntities ent = new ccarMeetingMinutesEntities();
+            proList = MeetingMinutesDatesDetails.fromMMUsersDB(ent.mMusers.ToList());
+            return View(proList);
+        }
+
+
+
+
+    }
+}
---
+++ ccar/Models/MeetingMinutesDatesDetails.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ccar.Models
+{
+    public class MeetingMinutesDatesDetails
+    {
+        public int id { get; set; }
+        public string firstname { get; set; }
+        public string surname { get; set; }
+        public string email { get; set; }
+
+        public string name { get { return firstname + " " + surname; } }
+
+        // wlasciowsci od meeting minutes attendance status
+        public string attendanceStatus { get; set; }
+        public static SelectList attendanceStatusList { get { return GetAttendanceStatusList(); } }
+
+        // wlasciwosci od meeting minutes project list
+        public static SelectList projectList { get { return GetProjectList(); } }
+
+        // get attendance status list
+        public static SelectList GetAttendanceStatusList()
+        {
+
+            ccarMeetingMinutesEntities ent = new ccarMeetingMinutesEntities();
+            var statusList = ent.MeetingMinutesAttendanceStatus.ToList();
+            return new SelectList(statusList, "id", "statusAttendance");
+        }
+
+        // get project list
+
+        private static SelectList GetProjectList()
+        {
+            ccarMeetingMinutesEntities ent = new ccarMeetingMinutesEntities();
+            var projectlist = ent.MeetingMinutesProjects.ToList();
+            return new SelectList(projectlist, "id", "projectName");
+        }
+
+        // List of reasons
+        public static List<MeetingMinutesDatesDetails> fromMMUsersDB(List<mMusers> uList)
+        {
+            List<MeetingMinutesDatesDetails> usersList = uList.Select(x => new MeetingMinutesDatesDetails() { id = x.id, firstname = x.firstname, surname = x.surname, email = x.email }).ToList();
+            return usersList;
+        }
+        public static List<MeetingMinutesDatesDetails> GetReasonList()
+        {
+            ccarMeetingMinutesEntities ent = new ccarMeetingMinutesEntities();
+            return fromMMUsersDB(ent.mMusers.ToList());
+        }
+
+    }
+}
