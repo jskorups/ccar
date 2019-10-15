e21e8aa3e7011b268a68fc6f9250828dbdb4c19e
Jakub Skorupski <skorupski.jakub@gmail.com>
2019-10-15 22:41:55 +0200

    15102019 #2

---
+++ ccar/ModelsMM/MMViewModels/AddMeetingViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ccar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ccar.ModelsMM.MMViewModels
+{
+    public class AddMeetingViewModel
+    {
+        public MeetingModel meeting { get; set; }
+        public List<ProjectModel> projList { get { return ProjectModel.GetProjectList(); } }
+        public List<UserModel> userList { get { return UserModel.GetUsersList(); } }
+    }
+    // 1 a (new addmeeting i przekazac do widoku w gecie), return view(model)
+    // 1. metody do zapisywania tego zmieniam na widoku (http post)
+    // 2. podpinanie kontrolek z modelu
+    // 3. textboxfor(x=>x.meeting.projectid)
+
+  
+
+}
