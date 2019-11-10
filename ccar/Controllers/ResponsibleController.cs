5d39f04b0ce0e403d12ef1d5565e153c49fd4ac0
Jakub Skorupski <skorupski.jakub@gmail.com>
2019-11-10 19:06:37 +0100

    10112019 #1

---
+++ ccar/Controllers/ResponsibleController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ccar.Models;
+
+namespace ccar.Controllers
+{
+    public class ResponsibleController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ResponsiblesList()
+        {
+            List<ResponsibleModel> responsibles = new List<ResponsibleModel>();
+            ccarEntities ent = new ccarEntities();
+            responsibles = ResponsibleModel.fromUsers(ent.responsibles.ToList());
+            return View(responsibles);
+        }
+
+        [HttpGet]
+        public ActionResult DropDownListOfResponsibles()
+        {
+            ccarEntities ent = new ccarEntities();
+            ViewBag.DepartmentList = new SelectList(ent.responsibles.ToList(), "id", "FirstName" + "Lastname", 1);
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ResponsibleModel res = new ResponsibleModel();
+            return View(res);
+        }
+        [HttpPost]
+        public ActionResult Create(ResponsibleModel u)
+        {
+            if (ModelState.IsValid)
+            {
+                ccarEntities ent = new ccarEntities();
+                ent.responsibles.Add(ResponsibleModel.ConvertFromModelToDB(u));
+                ent.SaveChanges();
+                return RedirectToAction("ResponsiblesList");
+            }
+            return View(u);
+
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            ccarEntities ent = new ccarEntities();
+            ResponsibleModel us = ResponsibleModel.ConvertFromDbToModel(ent.responsibles.Where(x => x.id == id).FirstOrDefault());
+            return View(us);
+        }
+        [HttpPost]
+        public ActionResult Edit(ResponsibleModel us)
+        {
+            if (ModelState.IsValid)
+            {
+                ccarEntities ent = new ccarEntities();
+                responsibles res = ent.responsibles.Where(x => x.id == us.id).FirstOrDefault();
+
+                res.id = Convert.ToInt32(us.id == null ? 0 : us.id);
+                res.FirstName = us.FirstName;
+                res.Lastname = us.Lastname;
+                res.email = us.email;
+                ent.SaveChanges();
+                return RedirectToAction("ResponsiblesList");
+            }
+            return View(us);
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            ccarEntities ent = new ccarEntities();
+            ResponsibleModel user = ResponsibleModel.ConvertFromDbToModel(ent.responsibles.Where(x => x.id == id).FirstOrDefault());
+            return View(user);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                ccarEntities ent = new ccarEntities();
+                responsibles user = ent.responsibles.Find(id);
+                ent.responsibles.Remove(user);
+                ent.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("DeleteRefused");
+            }
+            return RedirectToAction("ResponsiblesList");
+        }
+        [HttpGet]
+        public ActionResult DeleteRefused()
+        {
+            return View();
+        }
+    }
+
+
+
+
+}
---
+++ ccar/Models/ResponsibleModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ccar.Models
+{
+    public class ResponsibleModel
+    {
+
+
+
+
+            public int? id { get; set; }
+
+            [Required(ErrorMessage ="Required")]
+            [StringLength(10, ErrorMessage = "Maximum 10 characters.")]
+            public string FirstName { get; set; }
+
+            [Required(ErrorMessage = "Required")]
+            [StringLength(15, ErrorMessage = "Maximum 15 characters.")]
+            public string Lastname { get; set; }
+
+            [Required(ErrorMessage = "Required")]
+            [StringLength(50, ErrorMessage = "Maximum 30 characters.")]
+            [RegularExpression(@"^\w+([-+.']\w+)*@grupoantolin.com$", ErrorMessage = "Incorrect email. ")]
+            public string email { get; set; }
+
+            public string Name { get; set; }
+
+            //      1.pobrac adres mailowy na podtsawie ID act(w modelu initaiotor), get emailadress, zwraca maila
+            //2. sprawdzanie czy nie jest null i czy jest poprawny - w metodzie
+
+
+            public static string getEmailAdress(int? userId)
+            {
+                ccarEntities ent = new ccarEntities();
+                var xo = ent.responsibles.Where(x => x.id == userId).FirstOrDefault();
+                if (xo != null)
+                {
+                    return xo.email;
+                }
+                else
+                {
+                    return "";
+                }
+
+            }
+
+            public static List<ResponsibleModel> fromUsers(List<responsibles> userList)
+            {
+                List<ResponsibleModel> listUsers = userList.Select(x => new ResponsibleModel() { id = x.id, FirstName = x.FirstName, Lastname  = x.Lastname, email = x.email, Name = x.FirstName + " " + x.Lastname }).ToList();
+                return listUsers;
+            }
+            public static List<ResponsibleModel> getUsersList()
+            {
+                ccarEntities ent = new ccarEntities();
+                return fromUsers(ent.responsibles.ToList());
+            }
+
+            public static responsibles ConvertFromModelToDB(ResponsibleModel u)
+            {
+                responsibles us = new responsibles();
+                us.id = Convert.ToInt32(u.id == null ? 0 : u.id);
+                us.FirstName = u.FirstName;
+                us.Lastname = u.Lastname;
+                us.email = u.email;
+
+                return us;
+            }
+            public static ResponsibleModel ConvertFromDbToModel(responsibles u)
+            {
+                ResponsibleModel mod = new ResponsibleModel();
+                mod.id = u.id;
+                mod.FirstName = u.FirstName;
+                mod.Lastname = u.Lastname;
+                mod.email = u.email;
+
+                return mod;
+            }
+
+            public void Save()
+            {
+                if (this.id == 0)
+                {
+                    ccarEntities ent = new ccarEntities();
+                    ent.responsibles.Add(ResponsibleModel.ConvertFromModelToDB(this));
+                }
+            }       
+    }
+}
