09119018ce28a90e51f7248a341a97aa421744c2
Jakub Skorupski <skorupski.jakub@gmail.com>
2019-11-14 16:04:23 +0100

    14112019 #1

---
+++ ccar/ControllersMA/MeetingActionsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ccar.ControllersMA
+{
+    public class MeetingActionsController : Controller
+    {
+        // GET: MeetingActions
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
