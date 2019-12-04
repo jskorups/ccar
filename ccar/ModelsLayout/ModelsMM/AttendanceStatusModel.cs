dbcdbbd1ee0eb351221f2341b34b89ffd1419627
Jakub Skorupski <skorupski.jakub@gmail.com>
2019-12-04 15:40:23 +0100

    04122019 #1

---
+++ ccar/ModelsLayout/ModelsMM/AttendanceStatusModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ccar.ModelsMM
+{
+    public class AttendanceStatusModel
+    {
+        public int StatusAttendanceId { get; set; }
+        public int StatudAttendanceName  { get; set; }
+    }
+}
