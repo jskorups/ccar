a576a56b3cb0d7b1ba378b0e270cc5d5ec37aa4e
Jakub Skorupski <skorupski.jakub@gmail.com>
2019-12-23 19:03:00 +0100

    23122019 #3

---
+++ ccar/ControllerCCARprojects/CCARprojectsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ccar.ControllerCCARprojects
+{
+    public class CCARprojectsController : Controller
+    {
+        // GET: CCARprojects
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
