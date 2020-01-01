dc4d4707c799d9e2714cd6511489cf1ea7bf0f71
Jakub Skorupski <skorupski.jakub@gmail.com>
2020-01-02 00:02:56 +0100

    02012020 #1

---
+++ ccar/ControllerActionMeetings/ActionMeetingsController.cs
@@ -0,0 +1,29 @@
+﻿using ccar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ccar.ControllerActionMeetings
+{
+    public class ActionMeetingsController : Controller
+    {
+        // GET: ActionMeetings
+        public ActionResult Index()
+        {
+            return View();        
+        }
+        [HttpGet]
+        public ActionResult GetData()
+        {
+            using (ccarEntities ent = new ccarEntities())
+            {
+                List<ActionMeetingsModel> actList = new List<ActionMeetingsModel>();
+                //actList = ent.actionView.ToList();
+                actList = ActionMeetingsModel.fromActions(ent.actionsMeetings.ToList());
+                return Json(new { data = actList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ ccar/Models/ActionMeetingsModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ccar.Models
+{
+    public class ActionMeetingsModel
+    {
+        //public int idReason { get; set; }
+        public string reason { get; set; }
+        public DateTime originationDate { get; set; }
+        public int initiatorId { get; set; }
+        public string attendanceList { get; set; }
+
+
+        public static actionsMeetings fromDBtoModel (ActionMeetingsModel model)
+        {
+            ccarEntities ent = new ccarEntities();
+
+            actionsMeetings act = new actionsMeetings();
+            model.reason = ent.reasons.Where(x => x.id == act.id).Select(x => x.reason).SingleOrDefault();
+            act.originationDate = model.originationDate;
+            act.initiatorId = model.initiatorId;
+            act.attendanceList = model.attendanceList;
+
+            return act;
+
+        }
+
+        public static ActionMeetingsModel fromModelToDb(actionsMeetings act)
+        {
+            ccarEntities ent = new ccarEntities();
+
+            ActionMeetingsModel model = new ActionMeetingsModel();
+            model.reason = ent.reasons.Where(x => x.id == act.id).Select(x => x.reason).SingleOrDefault();
+            model.originationDate = act.originationDate;
+            model.initiatorId = act.initiatorId;
+            model.attendanceList = act.attendanceList;
+            return model;
+        }
+
+
+        public static List<ActionMeetingsModel> fromActions(List<actionsMeetings> actList)
+        {
+            ccarEntities ent = new ccarEntities();
+            List<ActionMeetingsModel> listActs = actList.Select(c => new ActionMeetingsModel() { reason = ent.reasons.Where(x =>x.id == c.idReason).Select(x=> x.reason).SingleOrDefault(), originationDate = c.originationDate, attendanceList = c.attendanceList, initiatorId = c.initiatorId}).ToList();
+            return listActs;
+        }
+
+
+
+    }
+
+    
+}
