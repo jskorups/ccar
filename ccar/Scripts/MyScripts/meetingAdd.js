ee7fd3e2086fc6d463b38c98d6a0b169a199e502
Jakub Skorupski <skorupski.jakub@gmail.com>
2020-01-06 13:44:12 +0100

    06012020 #1

---
+++ ccar/Scripts/MyScripts/meetingAdd.js
@@ -0,0 +1,24 @@
+﻿function SubmitForm(form) {
+    //$.validator.unobtrusive.parse(form);
+    //if ($(form).valid()) {
+        $.ajax({
+            type: "POST",
+            url: form.action,
+            data: $(form).serialize(),
+            success: function (data) {
+                //if (data.success) {
+                Popup.dialog('close');
+                dataTable.ajax.reload();
+
+                $.notify(data.message, {
+                    globalPosition: "top center",
+                    className: "success"
+
+                });
+
+                //}
+            }
+        });
+    //}
+    return false;
+}
