f08ccd6c8ab739bd62c7b21003c426b5d11f4478
Jakub Skorupski <skorupski.jakub@gmail.com>
2020-01-15 21:04:47 +0100

    15012020 #1

---
+++ ccar/Scripts/MyScripts/selector.js
@@ -0,0 +1,9 @@
+﻿$('#filter').editableSelect({
+    // enable filter
+    filter: true
+});
+//.on('select.editable-select', function (e, li) {
+//    $('#no-filter').val(li.attr('value'));
+//            });
+
+$('#filter').editableSelect({ filter: true, effects: 'slide' });
